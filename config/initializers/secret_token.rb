55ff1fd58c2aa0d3dc7f10a4844b37ccdd4b6f90
geoom <gmejia.fisi@gmail.com>
2013-11-02 01:29:07 -0500

    init simple structure for project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Startupplace::Application.config.secret_key_base = '5379ac07306189f272df4952434f511d5f1c0464b88391a3f6f66a61c82eb923e00cae6af45830339527e4a1812625ea1dd1472748a16ce0f9ff5f0ebf22adb6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Startupplace::Application.config.session_store :cookie_store, key: '_startupplace_session'
