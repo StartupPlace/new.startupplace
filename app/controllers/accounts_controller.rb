9bfca89c44f378efcae8e722a1a24d3d69da3204
geoom <gmejia.fisi@gmail.com>
2014-03-12 18:23:02 -0500

    refine single account page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,12 @@
 class AccountsController < ApplicationController
-	layout "dashboard"
+	layout "account"
   def show
   	@user = User.find_by! username: params[:username]
+  	if current_user
+  		@is_owner = current_user.username == params[:username] ? true : false
+  	else
+  		@is_owner = false
+  	end
   end
 end
 
